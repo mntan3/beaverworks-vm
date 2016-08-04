ba95796252b0180fbe857d8dd9a4e95f8d345dce
Michelle Tan <mtan123456789@gmail.com>
2016-08-04 09:31:36 -0400

    deleted stuff

--- simulator_setup (1).sh
+++
@@ -1,7 +0,0 @@
-sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
-
-wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
-
-sudo apt-get update
-
-sudo apt-get install ros-indigo-controller-manager ros-indigo-gazebo-ros-control ros-indigo-gazebo-ros-pkgs ros-indigo-joint-state-controller ros-indigo-effort-controllers
